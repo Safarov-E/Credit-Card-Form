cd643d5787321334529b1667b63097a56d6f68c7
Safarov <elkhan.safarov10@yandex.com>
2020-06-01 23:13:33 +0500

    Выделение активных элементов

--- src/components/card-form/card-form.js
+++ src/components/card-form/card-form.js
@@ -4,15 +4,22 @@ import visa from './img/visa.png';
 import './card-form.css';
 
 export default class extends React.Component {
-    state = {
-        cardMonth: [],
-        cardYear: [],
-        cardMonthValue: null,
-        cardYearValue: null,
-        cardName: '',
-        cardNumber: '',
-        cardNumber1: [],
-        visible: true
+    constructor(props){
+        super(props)
+        this.state = {
+            cardMonth: [],
+            cardYear: [],
+            cardMonthValue: null,
+            cardYearValue: null,
+            cardName: '',
+            cardNumber: '',
+            cardNumber1: [],
+            visible: true
+        }
+        this.myRef = React.createRef();
+        this.myRef_1 = React.createRef();
+        this.myRef_2 = React.createRef();
+        this.myRef_3 = React.createRef();
     }
     componentDidMount() {
         function arr(key, key_1) {
@@ -35,23 +42,41 @@ export default class extends React.Component {
     handlerSelectMonth = (event) => {
         let valueMonth = event.target.value;
         this.setState({ cardMonthValue: valueMonth })
+        this.myRef_2.current.classList.add('active');
+        this.myRef.current.classList.remove('active');
+        this.myRef_1.current.classList.remove('active');
     }
     handlerSelectYear = (event) => {
         let valueMonth = event.target.value;
         let year = valueMonth.split('').splice(-2, 2).join('');
         this.setState({ cardYearValue: year })
+        this.myRef_2.current.classList.add('active');
+        this.myRef.current.classList.remove('active');
+        this.myRef_1.current.classList.remove('active');
     }
     handlerInputName = (event) => {
         let valueName = event.target.value;
-        this.setState({ cardName: valueName })
+        this.myRef.current.classList.remove('active');
+        this.myRef_2.current.classList.remove('active');
+        if(valueName === '') {
+            this.myRef_1.current.classList.remove('active');
+            this.setState({ cardName: '' })
+        } else {
+            this.myRef_1.current.classList.add('active');
+            this.setState({ cardName: valueName })
+        }
     }
     handlerInputNumber = (event) => {
         let valueNumber = event.target.value;
+        this.myRef_1.current.classList.remove('active');
+        this.myRef_2.current.classList.remove('active');
         if(Number(valueNumber) && valueNumber.length <= 16) {
             console.log(valueNumber.length)
             this.setState({cardNumber: valueNumber})
+            this.myRef.current.classList.add('active');
         } else if(valueNumber.length <= 1) {
             this.setState({cardNumber: ''})
+            this.myRef.current.classList.remove('active');
         }
     }
     render() {
@@ -66,7 +91,7 @@ export default class extends React.Component {
                             <img src={visa} style={{width: '85px', height: '45px'}}/>
                         </div>
                         <label htmlFor="cardNumber" className="card-item__numbers">
-                            <div className="card-item__number">
+                            <div className="card-item__number" ref={this.myRef}>
                                 {   cardNumber === '' ?
                                         cardNumber1.map((item, i) => {
                                             return(
@@ -83,11 +108,11 @@ export default class extends React.Component {
                             </div>
                         </label>
                         <div className="card-item__content">
-                            <label htmlFor="cardName" className="card-item__info">
+                            <label htmlFor="cardName" className="card-item__info" ref={this.myRef_1}>
                                 <div className="card-item__holder">Card Holder</div>
                                 <div className="card-item__name">{cardName.trim() === '' ? 'Full Name' : cardName}</div>
                             </label>
-                            <div className="card-item__date">
+                            <div className="card-item__date" ref={this.myRef_2}>
                                 <label htmlFor="cardMonth" className="card-item__dateTitle">Expires</label>
                                 <div className="cardMonth_group">
                                     <label htmlFor="cardMonth" className="card-item__dateItem">
@@ -110,7 +135,7 @@ export default class extends React.Component {
                     </div>
                     <div className="cart-input_holders">
                         <label htmlFor="cardName" className="cart-label_holders">Card Holders</label>
-                        <input type="text" id="cardName" className="cardHolders" onChange={this.handlerInputName}/>
+                        <input type="text" id="cardName" value={cardName} className="cardHolders" onChange={this.handlerInputName}/>
                     </div>
                     <div className="card-form__row">
                         <div className="card-form__group">

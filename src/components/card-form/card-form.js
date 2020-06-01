9008178f32f17083870ffae919e94484cc61776d
Safarov <elkhan.safarov10@yandex.com>
2020-06-01 21:17:35 +0500

    Вывод номера карты

--- src/components/card-form/card-form.js
+++ src/components/card-form/card-form.js
@@ -9,22 +9,28 @@ export default class extends React.Component {
         cardYear: [],
         cardMonthValue: null,
         cardYearValue: null,
-        cardName: ''
+        cardName: '',
+        cardNumber: '',
+        cardNumber1: [],
+        visible: true
     }
     componentDidMount() {
-        let arr = [], count = 0;
-        for(let i = 2020; i <= 2031; i++) {
-            arr[count] = i;
-            count++;
+        function arr(key, key_1) {
+            let arr = [], count = 0;
+            for(let i = key; i <= key_1; i++) {
+                arr[count] = i;
+                count++;
+            }
+            return arr;
         }
-        this.setState({cardYear: arr})
+        this.setState({cardYear: arr(2020, 2031)})
+        this.setState({cardMonth: arr(1, 12)})
 
-        let arr1 = [], count1 = 0;
-        for(let i = 1; i <= 12; i++) {
-            arr1[count1] = i;
-            count1++;
+        let arr1 = [];
+        for(let i = 0; i < 16; i++) {
+            arr1[i] = '#';
         }
-        this.setState({cardMonth: arr1})
+        this.setState({cardNumber1: arr1})
     }
     handlerSelectMonth = (event) => {
         let valueMonth = event.target.value;
@@ -39,9 +45,18 @@ export default class extends React.Component {
         let valueName = event.target.value;
         this.setState({ cardName: valueName })
     }
+    handlerInputNumber = (event) => {
+        let valueNumber = event.target.value;
+        if(Number(valueNumber) && valueNumber.length <= 16) {
+            console.log(valueNumber.length)
+            this.setState({cardNumber: valueNumber})
+        } else if(valueNumber.length <= 1) {
+            this.setState({cardNumber: ''})
+        }
+    }
     render() {
-        const { cardMonth, cardYear, cardMonthValue, cardYearValue, cardName } = this.state;
-        console.log()
+        const { cardMonth, cardYear, cardMonthValue, cardYearValue, cardName, cardNumber, cardNumber1 } = this.state;
+        
         return (
             <div className="card-from">
                 <div className="card-item">
@@ -52,28 +67,19 @@ export default class extends React.Component {
                         </div>
                         <label htmlFor="cardNumber" className="card-item__numbers">
                             <div className="card-item__number">
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                            </div>
-                            <div className="card-item__number">
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                            </div>
-                            <div className="card-item__number">
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                            </div>
-                            <div className="card-item__number">
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
-                                <div className="card-item__numberItem">#</div>
+                                {   cardNumber === '' ?
+                                        cardNumber1.map((item, i) => {
+                                            return(
+                                                <div className="card-item__numberItem" key={i}>{item}</div>
+                                            )
+                                        })
+                                    :
+                                        cardNumber.split('').map((item, i) => {
+                                            return(
+                                                <div className="card-item__numberItem" key={i}>{item}</div>
+                                            )
+                                        })
+                                }
                             </div>
                         </label>
                         <div className="card-item__content">
@@ -99,15 +105,12 @@ export default class extends React.Component {
                 <form className="card-form__inner">
                     <div className="cart-input_number">
                         <label htmlFor="cardNumber" className="cart-label_number">Card Number</label>
-                        <input type="text" id="cardNumber" className="cardNumber" />
+                        <input type="text" id="cardNumber" className="cardNumber" onChange={this.handlerInputNumber}
+                        value={cardNumber}/>
                     </div>
                     <div className="cart-input_holders">
                         <label htmlFor="cardName" className="cart-label_holders">Card Holders</label>
-                        <input type="text" id="cardName" 
-                                className="cardHolders" 
-                                onChange={this.handlerInputName}
-                                value={cardName}
-                        />
+                        <input type="text" id="cardName" className="cardHolders" onChange={this.handlerInputName}/>
                     </div>
                     <div className="card-form__row">
                         <div className="card-form__group">
